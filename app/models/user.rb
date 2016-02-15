b75fe31f69b47d1a2801141114ba99b28f0f36e9
dgoore <darius@campgurus.com>
2016-02-15 15:09:15 -0500

    Fixing MAilchimp to pull in correct subscriber status
    currently user can subscribe and unsubscribe using the check box in the
    user profile create/edit form
    also updated hard coded text on the edit profile page
    need to confirm that status is updated appropriately for index page web
    based signups as wells
    also need to adjust content to reflect status
    and need to add rspec that tests but does not actually hit the api

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,73 @@ class User < ActiveRecord::Base
   end
 
   def add_to_mailchimp
-    if self.subscribed?
-      gb = Gibbon::API.new
-      gb.lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email => {:email => "#{self.email}"}, :merge_vars => {:FNAME => "#{self.name}"}})
+    gb = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+    if self.subscribed? # if the user has checked the "subscribed" checkbox
+      if self.mailchimp_status.present? # if MailChimp recognizes the email address
+        gb.lists(ENV['MAILCHIMP_LIST_ID']).members(Digest::MD5.hexdigest("#{self.email.downcase}")).update(body: { status: "subscribed" }) #subscribe user 
+      elsif self.mailchimp_status.nil? # if MailChimp doesn't have a user for the email address
+        gb.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: { # create a MailChimp pending subscriber
+          email_address: "#{self.email}",
+          status: "pending", # setting this to 'subscribed' will remove double optin
+          merge_fields: {FNAME: "#{self.name}"}
+          })
+      end
+    # unsubscribe user if box is unchecked but mailchimp has user as subscribed or pending
+    else
+      unless self.mailchimp_user.kind_of?(Array)
+        gb.lists(ENV['MAILCHIMP_LIST_ID']).members(self.mailchimp_member_id).update(body: { status: "unsubscribed" })  
+      end
     end
   end
+
+# -------- Previous code
+  # returns the mailchimp member if one exists for @user.email
+  def mailchimp_user
+    gb = Gibbon::Request.new
+    gb.lists(ENV['MAILCHIMP_LIST_ID']).members(Digest::MD5.hexdigest("#{self.email.downcase}")).retrieve
+    rescue Gibbon::MailChimpError => e
+    return nil, :flash => { error: e.message }
+  end
+  
+  # returns mailchimp member id for users registered with MC
+  def mailchimp_member_id
+    if self.mailchimp_user.kind_of?(Array)
+      return nil
+    elsif self.mailchimp_user.kind_of?(Hash)
+      self.mailchimp_user["id"]
+    end
+  end
+
+  # # returns the mailChimp status of the user  
+  def mailchimp_status
+    if self.mailchimp_user.kind_of?(Array)
+      return nil
+    elsif self.mailchimp_user.kind_of?(Hash)
+      self.mailchimp_user["status"]
+    end
+  end
+
+  # # syncs Mailchimp status to EBW, subscribing and unsubscribing users as appropriate. potential MC status includes
+  # # 'subscribed', 'unsubscribed', 'pending' and 'cleaned'
+  # def add_to_mailchimp
+  #   gb = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+  #   if self.subscribed? # if the user has checked the "subscribed" checkbox
+  #     if self.mailchimp_status.present? # if MailChimp recognizes the email address
+  #       gb.lists(ENV['MAILCHIMP_LIST_ID']).members(Digest::MD5.hexdigest("#{self.email.downcase}")).update(body: { status: "subscribed" }) #subscribe user 
+  #     elsif self.mailchimp_status.nil? # if MailChimp doesn't have a user for the email address
+  #       gb.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: { # create a MailChimp pending subscriber
+  #         email_address: "#{self.email}",
+  #         status: "pending", # setting this to 'subscribed' will remove double optin
+  #         merge_fields: {FNAME: "#{self.name}"}
+  #         })
+  #     end
+
+  # # unsubscribe user if box is unchecked but mailchimp has user as subscribed or pending
+  #   else
+  #     unless self.mailchimp_user.kind_of?(Array)
+  #       gb.lists(ENV['MAILCHIMP_LIST_ID']).members(self.mailchimp_member_id).update(body: { status: "unsubscribed" })  
+  #     end
+  #   end
+  # end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "returns the correct MailChimp status for the user email" do
+  end
+
+  it "adds a user to the newsletter if the 'subscribed' box is checked" do
+  end
+
+  it "returns subscribed for users who have subscribed via the MailChimp web form" do
+  end
+
+  it "unsubscribes the user from the newsletter if 'subscribed' box is unchecked" do
+  end
+end
