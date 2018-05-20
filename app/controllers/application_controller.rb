f9071048e20562eb9b9d3713f8f582f54a2b783a
Mark Nyon <mark@grandkru.com>
2018-05-20 09:08:22 +0000

    [WIP] Add devise logic to redirect
    - app/controllers/application_controller.rb - Following advice from
      https://github.com/plataformatec/devise/wiki/How-To:-
      Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
    
    Updates #99

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :mailbox, :conversation
 
+  before_action :store_user_location!, if: :storable_location?
+  # The callback which stores the current location must be added before you authenticate the user 
+  # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
+  # before the location can be stored.
+  before_action :authenticate_user!
+
   def info_for_paper_trail
     # Save additional info
     { ip: request.remote_ip }
@@ -43,9 +49,24 @@ class ApplicationController < ActionController::Base
     redirect_to '/'
   end
 
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+  #    infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
   def state_objects
     @state_objects ||= State.all
   end
+
 
   protected
 
